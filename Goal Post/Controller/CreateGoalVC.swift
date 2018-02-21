7cd5bc5ab2616933e2d2837839318aca6fdffd57
vinoth <vinothvino42@gmail.com>
2018-02-21 23:46:20 +0530

    CreateGoal View Controller added

---
+++ Goal Post/Controller/CreateGoalVC.swift
@@ -0,0 +1,40 @@
+//
+//  CreateGoalVC.swift
+//  Goal Post
+//
+//  Created by Vinoth Vino on 21/02/18.
+//  Copyright © 2018 Coder Earth. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func nextBtnPressed(_ sender: Any) {
+        
+    }
+    @IBAction func shortTermBtnPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func longTermBtnPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
